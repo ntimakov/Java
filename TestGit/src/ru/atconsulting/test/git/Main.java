caf498589b0bb77517f72aff4d49bec03f7b8f18
ntimakov <nicktimakov@mail.ru>
2016-06-29 16:32:32 +0300

    aaaa

---
+++ TestGit/src/ru/atconsulting/test/git/Main.java
@@ -0,0 +1,11 @@
+package ru.atconsulting.test.git;
+
+/**
+ * Created by ntimakov on 29.06.2016.
+ */
+public class Main {
+
+    public void main(String[] args){
+        System.out.println("TEST!");
+    }
+}
